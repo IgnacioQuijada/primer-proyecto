5707b3a39f3eec466f2f066f068b584fbeb9bccf
ignacio quijad <ignacio.quijada06@nmacapmail.cl>
2023-09-25 16:45:34 -0300

    agregar elementos

--- universo.py
+++ universo.py
@@ -1,2 +1,16 @@
-class unidimensional:
-    pass
+FROM abc import ABC, abstractclassmethod
+class unidimensional(ABC):
+  
+  def __init__(self):
+      self.volumen=0
+      self.masa=0
+      print("soy unidimensional")
+    
+
+
+
+
+if(__name__=="__main__"):
+    print("estoy en el programa principal")
+    c = unidimensional()
+    
